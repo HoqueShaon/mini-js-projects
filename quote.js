4a53147d84b0a794b55017ca31062149ef6c339d
HoqueShaon <cse225jvn@gmail.com>
2025-12-10 01:40:49 +0600

    quotes is running good

--- quote.js
+++ quote.js
@@ -10,6 +10,15 @@ const quotes =[
     "Do what you can, with what you have, where you are.",
     "Imagination is more important than knowledge.",
     "To be, or not to be: that is the question.",
-"The future belongs to those who believe in the beauty of their dreams.",
-"In three words I can sum up everything I've learned about life: it goes on."
+    "The future belongs to those who believe in the beauty of their dreams.",
+    "In three words I can sum up everything I've learned about life: it goes on."
 ]
+
+const usedIndexes = new Set()
+const quoteElement = document.getElementById('quote')
+
+function generateQuote(){
+    const randomIdx = Math.floor(Math.random() * quotes.length )
+    const quote = quotes[randomIdx];
+    quoteElement.innerHTML = quote;
+}
